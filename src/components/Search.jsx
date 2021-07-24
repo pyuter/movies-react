66c254632d629b95072ed7f1d9b706a5b886fc66
Pyuter <yukrop.94@gmail.com>
2021-07-25 00:50:48 +0300

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+class Search extends React.Component {
+  state = {
+    search: '',
+    type: 'all',
+  };
+
+  hendleKey = (event) => {
+    if (event.key === 'Enter') {
+      this.props.searchMovies(this.state.search, this.state.type);
+    }
+  };
+
+  handleFilter = (event) => {
+    this.setState(() => ({ type: event.target.dataset.type }), () => {
+      this.props.searchMovies(this.state.search, this.state.type);
+    });
+  };
+
+  render() {
+    return (
+      <div className='row'>
+        <div className='input-field col s12'>
+          <input
+            id='search'
+            type='search'
+            placeholder='search'
+            className='validate'
+            value={this.state.search}
+            onChange={(e) => this.setState({ search: e.target.value })}
+            onKeyDown={this.hendleKey}
+          />
+          <button
+            className='btn search-btn'
+            onClick={() => this.props.searchMovies(this.state.search)}
+          >
+            Search
+          </button>
+        </div>
+        <div>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='all'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'all'}
+            />
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='movie'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'movie'}
+            />
+            <span>Movies only</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='series'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'series'}
+            />
+            <span>Series Only</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='episode'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'episode'}
+            />
+            <span>Episode Only</span>
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Search };
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header () {
+    return  <nav className="teal darken-3">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Readt Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">Repo</a></li>
+      </ul>
+    </div>
+  </nav>
+}
+
+export {Header}
